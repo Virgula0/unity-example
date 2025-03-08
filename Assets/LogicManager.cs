93d3897f7d61eb4573def9e548b967a3108839d1
virgula <angelo.rosa@protonmail.com>
2025-03-08 11:02:26 +0100

    gameArea prefab initialization with instantiate in logic manager

--- Assets/LogicManager.cs
+++ Assets/LogicManager.cs
@@ -12,6 +12,7 @@ public class LogicManager : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public GameObject gameAreaPrefab;
 
     [ContextMenu("IncreaseScore") ] // this let the function to be run from unity, this will be found as function in the 3 dot in logic script in inspector to check the increase for debugging
     public void addScore(int score){
@@ -24,6 +25,8 @@ public class LogicManager : MonoBehaviour
     }
 
     void Start(){
+         // initialize GameArea using the prefab transform position within unity inspector
+        Instantiate(gameAreaPrefab, gameAreaPrefab.transform.position, gameAreaPrefab.transform.rotation);
         this.playerScore = 0;
         scoreText.text = $"{playerScore}";
     }
