64b1beba8d88efd00bb86a7629b21e4cf1bcd741
Tycho4311 <76877590+Tycho4311@users.noreply.github.com>
2025-03-16 22:24:26 +0100

    added gun and bullets with simple logic to build upon (#3)
    * added guna and bullets with simple logic to build upon
    
    * Changed from time to position the control for destroying bullets, cleaned up the code.
    
    * removing play on awake for screaming audio
    
    * code rafactoring and logic improvements
    
    * removing useless imports
    
    * removing space
    
    ---------
    
    Co-authored-by: Virgula <ängelo.rosa@protonmail.com>

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private float DESTROY_X_AXIS;
+    private GunScript gunScript;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.gunScript = GameObject.FindGameObjectWithTag("FirstGun").GetComponent<GunScript>(); // Find gun prefab by tag
+
+        if (!this.gunScript){
+            throw new ArgumentNullException("gun script should not be null");
+        }
+
+        this.DESTROY_X_AXIS = gunScript.getPipeSpawner().transform.position.x; // for some reason pipeSpawner object cannot be attached to Bullet Prefab
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > DESTROY_X_AXIS){
+            Destroy(this.gameObject);
+        } else {
+            transform.position = MoveBullet();
+        }
+    }
+
+    private Vector3 MoveBullet(){
+        return transform.position + Vector3.right * this.gunScript.getSpeed() * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.layer == (int)Utils.Objects.Pipe)
+        {
+            Debug.Log("BULLET DESTROYED BY PIPE");
+            Destroy(this.gameObject);
+        }
+    }
+}
